7afa390c67e0e134ecb686a61059c86705981af5
vitor <vitorgabriel181004@gmail.com>
2025-01-17 11:04:13 -0300

    feat: implementando decorator para post em users/create

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,17 +1,42 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { randomUUID } from "crypto";
 
 
 type ParamsUsers = {
     id: string;
 }
 
+type QueryUsers = {
+    p: string;
+    r: string;
+}
+
+type BodyUsers = {
+    name: string;
+    age: number;
+}
 
 
-@Controller()
+
+@Controller("/users")
 export class UserController {
 
-    @Get("/users/:id")
+    @Get("/:id")
     findById(@Param() params: ParamsUsers) {
         return "Usuario com id " + params.id;
     }
+
+    @Get("/findByPages")
+    findByPages(@Query() query: QueryUsers) {
+        return 'Query: ' + query.p + ' ' + query.r;
+    }
+
+    @Post("/create")
+    create(@Body() data: BodyUsers) {
+        return {
+            ...data,    
+            id: randomUUID()
+        }
+    }
+
 }
